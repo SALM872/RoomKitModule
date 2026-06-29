ba96194070fbe35c45ea59bfeaa238d28c852ee6
SALM872 <salmankhn872@gmail.com>
2026-06-29 14:49:39 +0530

    Update build.gradle.kts

--- roomkit/build.gradle.kts
+++ roomkit/build.gradle.kts
@@ -1,7 +1,7 @@
 ﻿plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28"
     id("maven-publish")
 }
 
@@ -10,17 +10,17 @@ android {
     compileSdk = 36
     defaultConfig { minSdk = 26 }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "11" }
+    kotlinOptions { jvmTarget = "17" }
     publishing { singleVariant("release") { withSourcesJar() } }
 }
 
 dependencies {
-    api("androidx.room:room-runtime:2.6.1")
-    api("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    api("androidx.room:room-runtime:2.7.2")
+    api("androidx.room:room-ktx:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     api("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
@@ -33,7 +33,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.SALM872"
                 artifactId = "RoomKitModule"
-                version = "1.0.0"
+                version = "1.1.0"
             }
         }
     }
